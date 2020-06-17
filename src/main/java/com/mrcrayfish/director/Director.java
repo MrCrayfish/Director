f69ab6016edcb9d514052beec43ce48bb2a7afbf
MrCrayfish <mrcrayfish@hotmail.com>
2020-06-17 22:04:47 +0930

    Removed example project and created bare mod

---
+++ src/main/java/com/mrcrayfish/director/Director.java
@@ -0,0 +1,15 @@
+package com.mrcrayfish.director;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod("director")
+public class Director
+{
+    public Director()
+    {
+
+    }
+}
